f66174bae8ffd335a2027da0aa1ba5e25164b32e
Michael Meuli <michael.meuli@gmail.com>
2022-04-18 07:19:44 +0200

    add source code

--- pages/TechStack.vue
+++ pages/TechStack.vue
@@ -3,23 +3,26 @@
     <p>&nbsp;</p>
     <h4>Backend</h4>
     <p>
-      <a href="https://www.vendure.io/">vendure.io</a>
+      <a href="https://www.vendure.io/" target="_blank">vendure.io</a>
     </p>
     <p>
       A modern, open-source headless commerce framework built with TypeScript &
       Nodejs
     </p>
     <p>
-      <a href="https://github.com/michaelmeuli/yl-vendure">Source code</a>
+      <a href="https://demo.vendure.io/" target="_blank">Vendure Demo</a>
+    </p>
+    <p>
+      <a href="https://github.com/michaelmeuli/yl-vendure" target="_blank">Source code</a>
     </p>
     <p>&nbsp;</p>
     <h4>Frontend</h4>
     <p>
-      <a href="https://docs.vuestorefront.io/vendure/">Vue Storefront</a>
+      <a href="https://docs.vuestorefront.io/vendure/" target="_blank">Vue Storefront</a>
     </p>
     <p>Lightning-Fast Frontend Platform for Headless Commerce</p>
     <p>
-      <a href="https://github.com/michaelmeuli/template-vendure">Source code</a>
+      <a href="https://github.com/michaelmeuli/template-vendure" target="_blank">Source code</a>
     </p>
   </div>
 </template>
