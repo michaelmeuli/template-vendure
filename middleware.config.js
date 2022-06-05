051b832b3354cb9fa5cd8184d9984b24eb918f02
Michael Meuli <michael.meuli@gmail.com>
2022-06-05 10:12:47 +0200

    add extension to middleware.config.js

--- middleware.config.js
+++ middleware.config.js
@@ -7,7 +7,22 @@ module.exports = {
           uri: process.env.GRAPHQL_API,
           tokenMethod: process.env.TOKEN_METHOD
         },
-      }
+      },
+      extensions: (extensions) => [
+        ...extensions,
+        {
+          name: 'stripe-extension',
+          extendApiMethods: {
+            setStripePayment: async (context) => {
+              const request = await context.client.mutate({
+                mutation: gql`mutation {createStripePaymentIntent}`,
+                fetchPolicy: NO_CACHE_FETCH_POLICY
+              });
+              return request.data;
+            }
+          }
+        }
+      ],
     }
   }
 };
