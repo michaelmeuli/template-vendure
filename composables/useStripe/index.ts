f0931a8f3e647eb0b4e4744aaba163cc42475191
Michael Meuli <michael.meuli@gmail.com>
2022-07-14 20:06:13 +0200

    wip

--- composables/useStripe/index.ts
+++ composables/useStripe/index.ts
@@ -2,11 +2,11 @@ import { computed } from '@vue/composition-api';
 import { sharedRef, useVSFContext,  } from '@vue-storefront/core';
 
 export const useStripe = () => {
-    const { $vendure } = useVSFContext();
+    const context = useVSFContext();
     const secret= sharedRef({}, 'useStripe');
     const set = async () => {
         console.log('going to call setStripePayment');
-        secret.value = await $vendure.api.setStripePayment();
+        secret.value = await context.$vendure.api.setStripePayment();
         console.log('secret.value: ', secret.value);
          };
 
